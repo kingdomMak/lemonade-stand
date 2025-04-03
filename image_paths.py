6ee9462cdfd365730dbe4b4e1f0a97d64fcd6275
Tati Software <tatisoftware_kingdom@Tatis-MacBook-Air.local>
2025-04-03 15:19:33 +0200

    🍋 First commit of my lemonade stand app

---
+++ image_paths.py
@@ -0,0 +1,16 @@
+# Dictionary of image paths for easy reference
+DRINK_IMAGES = {
+    'classic_lemonade': 'drinks/classic-lemonade.jpg',
+    'strawberry_lemonade': 'drinks/strawberry-lemonade.jpg',
+    'blueberry_lemonade': 'drinks/blueberry-lemonade.jpg'
+}
+
+BACKGROUND_IMAGES = {
+    'hero': 'background/hero-image.jpg',
+    'menu_bg': 'background/menu-background.jpg'
+}
+
+LOGO_IMAGES = {
+    'main_logo': 'logo/main-logo.png',
+    'favicon': 'logo/favicon.ico'
+} 
